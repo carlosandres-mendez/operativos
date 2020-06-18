4526cea2bfa1ee254a520a1e0d87603f7917420c
carlosandres-mendez <carlosandresucr@gmail.com>
2020-06-18 12:07:53 -0600

    Se mejora la identación de los ejemplos

--- ch4/EjemploThread.java
+++ ch4/EjemploThread.java
@@ -1,37 +1,32 @@
 // https://www.geeksforgeeks.org/multithreading-in-java/
 // Java code for thread creation by extending 
 // the Thread class 
-class MultithreadingDemo extends Thread 
-{ 
-        @Override
-	public void run() 
-	{ 
-		try
-		{ 
-			// Displaying the thread that is running 
-			System.out.println ("Thread " + 
-				Thread.currentThread().getId() + 
-				" is running"); 
 
-		} 
-		catch (Exception e) 
-		{ 
-			// Throwing an exception 
-			System.out.println ("Exception is caught"); 
-		} 
-	} 
-} 
+class MultithreadingDemo extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            // Displaying the thread that is running 
+            System.out.println("Thread "
+                    + Thread.currentThread().getId()
+                    + " is running");
+
+        } catch (Exception e) {
+            // Throwing an exception 
+            System.out.println("Exception is caught");
+        }
+    }
+}
 
 // Main Class 
-public class EjemploThread 
-{ 
-	public static void main(String[] args) 
-	{ 
-		int n = 8; // Number of threads 
-		for (int i=0; i<n; i++) 
-		{ 
-			MultithreadingDemo object = new MultithreadingDemo(); 
-			object.start(); 
-		} 
-	} 
-} 
+public class EjemploThread {
+
+    public static void main(String[] args) {
+        int n = 8; // Number of threads 
+        for (int i = 0; i < n; i++) {
+            MultithreadingDemo object = new MultithreadingDemo();
+            object.start();
+        }
+    }
+}
