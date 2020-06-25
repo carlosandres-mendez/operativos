36435464c4b1d74263b12388e3abac32ea00d46d
carlosandres-mendez <carlosandresucr@gmail.com>
2020-06-25 12:25:26 -0600

    Otros ejemplos de sincronización

---
+++ com/ProducerConsumerTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * Copyright (c) 1995-1997 Sun Microsystems, Inc. All Rights Reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software
+ * and its documentation for NON-COMMERCIAL purposes and without
+ * fee is hereby granted provided that this copyright notice
+ * appears in all copies. Please refer to the file "copyright.html"
+ * for further important copyright and licensing information.
+ *
+ * SUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+class ProducerConsumerTest {
+    public static void main(String[] args) {
+        CubbyHole c = new CubbyHole();
+        Producer p1 = new Producer(c, 1);
+        Consumer c1 = new Consumer(c, 1);
+
+        p1.start();
+        c1.start();
+    }
+}
