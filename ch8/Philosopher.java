4b3f4430ae94d57a648c5e8ba6d13d49a1c09cca
cmendez <carlosandresucr@gmail.com>
2021-07-29 20:06:15 -0600

    Se agrega ejemplo Filosofos comelones

---
+++ ch8/Philosopher.java
@@ -0,0 +1,58 @@
+/*
+ * https://www.baeldung.com/java-dining-philoshophers
+ */
+
+public class Philosopher implements Runnable {
+ 
+    // Member variables, standard constructor
+    // The forks on either side of this Philosopher 
+    private Object leftFork;
+    private Object rightFork;
+ 
+    public Philosopher(Object leftFork, Object rightFork) {
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+ 
+    private void doAction(String action) throws InterruptedException {
+        System.out.println(
+          Thread.currentThread().getName() + " " + action);
+        Thread.sleep(((int) (Math.random() * 100)));
+    }
+ 
+   // Member variables, methods defined earlier
+ 
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                
+                // thinking
+                doAction(System.nanoTime() + ": Thinking");
+                synchronized (leftFork) {
+                    doAction(
+                      System.nanoTime() 
+                        + ": Picked up left fork");
+                    synchronized (rightFork) {
+                        // eating
+                        doAction(
+                          System.nanoTime() 
+                            + ": Picked up right fork - eating"); 
+                        
+                        doAction(
+                          System.nanoTime() 
+                            + ": Put down right fork");
+                    }
+                    
+                    // Back to thinking
+                    doAction(
+                      System.nanoTime() 
+                        + ": Put down left fork. Back to thinking");
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return;
+        }
+    }
+}
